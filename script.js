79499620d8ec9c429012c200fb4620ed68124968
Mohamed Essam <63739425+mouhameedessam@users.noreply.github.com>
2022-03-17 01:19:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const menuSelect = document.querySelector('.menu-item'), 
+      menuParagraph = menuSelect.children,
+      foodItem = document.querySelectorAll('.food-item');
+
+for (let i = 0; i < menuParagraph.length; i++){
+  menuParagraph[i].addEventListener('click', () =>{
+    for(let j = 0; j < menuParagraph.length; j++){
+      menuParagraph[j].classList.remove('active');
+    }
+    menuParagraph[i].classList.add('active');
+    let filterValue = menuParagraph[i].getAttribute('data-filter')
+    for (let k =0; k < foodItem.length; k++){
+      if (filterValue === foodItem[k].getAttribute('data-category')){
+        foodItem[k].classList.remove('hide');
+        foodItem[k].classList.add('show');
+      } else{     
+        foodItem[k].classList.remove('show');
+        foodItem[k].classList.add('hide');
+      }
+      if (filterValue === 'all'){
+        foodItem[k].classList.remove('hide');
+        foodItem[k].classList.add('show');
+      }
+    }
+  })
+
+}
+
+const bookBtn = document.querySelector('.btn'),
+      modelBg = document.querySelector('.model-bg'),
+      close = document.querySelector('.close');
+      
+
+bookBtn.addEventListener('click', function(e){
+   modelBg.classList.add('bg-active');
+  e.preventDefault();
+});
+
+close.addEventListener('click', (e) =>{
+  modelBg.classList.remove('bg-active');
+  e.preventDefault();
+})
+
+
+const bars = document.querySelector('.burger');
+const ul = document.querySelector('.nav-links');
+const li = document.querySelectorAll('.nav-links li');
+
+
+bars.addEventListener('click', (e) =>{
+  
+  ul.classList.toggle('active');
+  // bars.classList.toggle('toggle')
+  
+  e.preventDefault();
+})
